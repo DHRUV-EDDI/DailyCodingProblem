a56fccb57009a9e724c75145a0f1168b7241ee74
DHRUV-EDDI <dhruv2018570@gmail.com>
2020-05-02 20:55:29 +0530

    9th-problem-modified

--- Problem-9/Problem-9.cpp
+++ Problem-9/Problem-9.cpp
@@ -2,38 +2,31 @@
 using namespace std;
 using ll = long long;
 vector<ll> v;
-ll  dp[1002],n;
-ll maxSum(ll i, ll prev) //Backtracking 
+ll n;
+ll maxSumNoAdj()
 {
-    if(i >= n) return 0;
-    ll excluding = maxSum(i+1,prev),including = 0;
-    if(prev + 1 != i)
-        including = v[i] + maxSum(i+1,i);
-    return max(including,excluding);
-}
-ll maxSumDP()
-{
-    dp[0] = v[0];
+    if(n == 1)
+        return v[0];
     if(n == 2)
-        dp[1] = max(v[0],v[1]);
-    
+        return max(v[0],v[1]);
+    ll frst = v[0],scnd = max(v[0],v[1]);
     for(int i=2;i<n;i++)
     {
-        dp[i] = max(dp[i-1],dp[i-2]+v[i]);
-        dp[i] = max(dp[i],v[i]);
+        ll temp = max(frst+v[i],scnd);
+        temp = max(temp,v[i]);
+        frst = scnd;
+        scnd = temp;
     }
-    return dp[n-1];
+    return scnd;
 }
 int main()
 {
     cin>>n;
-    for(int i=0;i<n;i++) 
-        dp[i] = INT_MIN;
     for(int i=0;i<n;i++)
     {
         ll x; cin >> x;
         v.push_back(x);
     }
-    cout<<maxSumDP();
+    cout<<maxSumNoAdj();
     return 0;
 }
