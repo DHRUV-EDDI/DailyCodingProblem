c7746b43753075174ac361fc1753994c2820b31b
DHRUV-EDDI <dhruv2018570@gmail.com>
2020-05-10 17:32:08 +0530

    Update-16th-problem

--- Problem-16/Problem-16.cpp
+++ Problem-16/Problem-16.cpp
@@ -2,49 +2,32 @@
 using namespace std;
 using ll = long long;
 
-bool isOperator(string k)
-{
-    if(k == "*" || k == "+" || k == "-" || k == "/")
-        return true;
-    return false;
-}
-ll operation(ll val1, ll val2, string op)
-{
-    if(op == "+")
-        return val1+val2;
-    else if(op == "*")
-        return val1*val2;
-    else if(op == "-")
-        return val2-val1;
-    else
-        return val2/val1;
-}
 int main()
 {
-    vector<string> v;
-    string s;
-    while(cin >> s)
+    ll n,k,strt=0;
+    cin >> n >> k;
+    vector<ll> v;
+    for(int i=0;i<n;i++)
     {
-        v.push_back(s);
+        ll x; cin >> x;
+        v.push_back(x);
     }
-    stack<ll> st;
-    for(int i=0;i<v.size();i++)
+    deque<ll> dq;
+    for(int i=0;i<k;i++)
     {
-        if(!isOperator(v[i]))
-        {
-            stringstream ss(v[i]);
-            ll val;
-            ss >> val;
-            st.push(val);   
-        }
-        else
-        {
-            ll val1 = st.top();st.pop();
-            ll val2 = st.top();st.pop();
-            st.push(operation(val1,val2,v[i]));
-            //cout<<st.top()<<":";
-        }
+        while(!(dq.empty()) && v[i] >= v[dq.front()])
+            dq.pop_back();
+        dq.push_back(i);
     }
-    cout<<st.top();
+    for(int i=k;i<n;i++)
+    {
+        cout<<v[dq.front()]<<" ";
+        while(!(dq.empty()) && dq.front() <= (i-k) )
+            dq.pop_front();
+        while( !(dq.empty()) && v[i] >= v[dq.front()])
+            dq.pop_back();
+        dq.push_back(i);
+    }
+    cout<<v[dq.front()]<<" ";
     return 0;
 }
